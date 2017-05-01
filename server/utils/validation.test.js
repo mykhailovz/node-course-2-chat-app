0ca953ad52538c98ce63c4048417160e15653ee7
Mykhailo <mykhailo.vz@gmail.com>
2017-05-01 04:50:29 -0400

    Add users class and updateUserList event

---
+++ server/utils/validation.test.js
@@ -0,0 +1,22 @@
+var mocha = require('mocha');
+var expect = require('expect');
+
+const {isRealString} = require('./validation');
+
+
+describe('isRealString', () => {
+    it('shoult reject non-string values', () => {
+        var res = isRealString(98);
+        expect(res).toBe(false);
+    }); 
+
+    it('should reject string with only spaces', () => {
+        var res = isRealString('         ');
+        expect(res).toBe(false);
+    });
+
+    it('should allow string with non-space characters', () => {
+        var res = isRealString('             Andrew           ');
+        expect(res).toBe(true);
+    });
+});
